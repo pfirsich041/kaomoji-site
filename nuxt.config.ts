c2860f099ec44a25f4ed737d7c309bf97dc257a8
Momoka Taniai <pfirsich.041@gmail.com>
2025-10-14 13:04:41 +0900

    add description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,18 @@ export default defineNuxtConfig({
   build: {
     transpile: []
   },
+  app: {
+    head: {
+      title: 'かわいい顔文字コレクション',
+      meta: [
+        { name: 'description', content: 'SNSへの投稿に使えるかわいい顔文字です。クリックでコピーできます★' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { charset: 'utf-8' },
+        // OGP設定
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: 'かわいい顔文字コレクション' },
+        { property: 'og:url', content: 'https://kawaiikaomoji.netlify.app/' },
+      ]
+    }
+  },
 })
