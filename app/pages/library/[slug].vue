a30498e6a383a2b6cae8216384eb864f4c7f879c
momoka taniai <pfirsich041@gmail.com>
2025-12-16 17:46:26 +0900

    顔文字の追加

---
+++ app/pages/library/[slug].vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="min-h-screen bg-pink-50">
+		<Header></Header>
+		<div class="min-h-screen">
+			<Library :category="$route.params.slug" />
+		</div>
+		<Footer></Footer>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
